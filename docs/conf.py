273d56884a02f069fdc869a716f28348148b25c3
ATATC <futerry@outlook.com>
2024-01-24 11:58:03 -0500

    Routine updates.

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'LEADS'
-copyright = '2024, ProjectNeura'
-author = 'ProjectNeura'
+project = "LEADS"
+copyright = "ProjectNeura"
+author = "ProjectNeura"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
