f97e3d8fc67c6f19641be3dcdb3727e8fb595b77
ATATC <futerry@outlook.com>
2024-04-13 19:08:21 -0400

    Routine updates.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "ProjectNeura"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_design"]
+extensions = ["myst_parser", "sphinx_design", "sphinx.ext.autodoc"]
 myst_enable_extensions = ["colon_fence"]
 
 templates_path = ["_templates"]
@@ -22,7 +22,18 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_logo = "_static/logo.png"
 html_favicon = "_static/logo.png"
+html_theme_options = {
+    "home_page_in_toc": True,
+    "github_url": "https://github.com/ProjectNeura/leads-docs",
+    "repository_url": "https://github.com/ProjectNeura/leads-docs",
+    "repository_branch": "master",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_edit_page_button": False,
+    "use_issues_button": True,
+    "announcement": "Docs to be completed",
+}
