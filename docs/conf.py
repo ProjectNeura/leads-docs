2ceb23a11c4b8fcd319d8c4211d8637556887eed
ATATC <futerry@outlook.com>
2024-04-13 19:00:09 -0400

    Routine updates.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@ author = "ProjectNeura"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "sphinx_design"]
+myst_enable_extensions = ["colon_fence"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -23,3 +24,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
